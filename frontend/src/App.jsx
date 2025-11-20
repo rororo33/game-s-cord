d9c7a6c792cddbf58116cd4e7a58a2e11613cb45
DESKTOP-AECKEEJ\jinsa <jsa0037@gmail.com>
2025-11-21 02:44:02 +0900

    feat: add page route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Template from "./template/Template";
 import Register from "./Register/Register";
 import Search from "./page/Search";
 import MyPage from "./page/MyPage/MyPage";
+import Login from "./login/Login";
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Register />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/search" element={<Search/>} />
           <Route path="/Mypage" element={<MyPage />} />
         </Routes>
--- frontend/src/template/Header.jsx
+++ frontend/src/template/Header.jsx
@@ -157,7 +157,7 @@ const Header = () => {
           <img src={coin} alt="coin" className={styles.coin}></img>
           <span>충전</span>
         </Link>
-        <Link className={`${styles.link} ${styles.login}`} to="/">
+        <Link className={`${styles.link} ${styles.login}`} to="/login">
           Login
         </Link>
         <Link className={`${styles.link} ${styles.join}`} to="/signup">
