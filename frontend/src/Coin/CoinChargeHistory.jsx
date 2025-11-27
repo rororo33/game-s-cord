6fa0be595a8be3b98fb38780926671da017c2dad
YujinB <yujinb0201@gmail.com>
2025-11-28 00:57:58 +0900

    fix : API url
    - fix API url

--- frontend/src/Coin/CoinChargeHistory.jsx
+++ frontend/src/Coin/CoinChargeHistory.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import api from "../api/axios"
+import api from "../api/axios";
 import HeaderTabs from "./HeaderTabs";
 import PaymentItem from "./PaymentItem";
 import "../css/CoinChargeHistory.css";
