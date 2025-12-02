3832464313f854f278229b8f0d0ac27eed3b26bd
rororo33 <sjznsjx25792@gmail.com>
2025-12-03 01:59:39 +0900

    fix : userDTO, Domain 수정

--- backend/src/main/java/com/example/gamescord/dto/user/UserSignupRequestDTO.java
+++ backend/src/main/java/com/example/gamescord/dto/user/UserSignupRequestDTO.java
@@ -21,7 +21,7 @@ public class UserSignupRequestDTO {
     private String loginId;
 
     @NotBlank(message = "비밀번호는 필수입니다")
-    @Size(min = 8, max = 255, message = "비밀번호는 8~255자 사이여야 합니다")
+    @Size(min = 8, max = 20, message = "비밀번호는 8~20자 사이여야 합니다")
     private String loginPwd;
 
     @NotBlank(message = "이메일은 필수입니다.")
