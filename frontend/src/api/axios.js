e29d753a6ea19d537963fcf28f71f55660a5f3fe
gihyun0429 <jsa0037@gmail.com>
2025-12-03 20:19:15 +0900

    fix : fix api header token

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,5 +3,17 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
